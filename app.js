7580241b6f83cc47c58f53eab731aa1e69d9b864
borkarsaish65 <borkarsaish7@gmail.com>
2020-02-06 22:56:12 +0530

    made the code more concise

--- app.js
+++ app.js
@@ -1,17 +1,11 @@
-const request = require('request');
 const geocode = require('./util/geocode');
 const forecast = require('./util/forecast');
-
-
 
 geocode('Pune',(error,data)=>{
 console.log('Error',error);
 console.log('Data',data);
 
 })
-
-
-
 forecast(18.52361,73.84778,(error, data) => {
     console.log('Error', error)
     console.log('Data', data)
