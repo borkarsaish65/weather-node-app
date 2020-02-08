50227ea864a374a021009cbb533ae4b0eb8d154c
borkarsaish65 <borkarsaish7@gmail.com>
2020-02-08 14:06:02 +0530

    callback chained

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const geocode = require('./util/geocode');
 const forecast = require('./util/forecast');
 
-geocode('Pune',(error,data)=>{
+geocode('Margao',(error,data)=>{
 console.log('Error',error);
 console.log('Data',data);
+forecast(data.latitude,data.longitude,(error, data) => {
+  console.log('Error', error)
+  console.log('Data', data)
+})
 
 })
-forecast(18.52361,73.84778,(error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-  })
