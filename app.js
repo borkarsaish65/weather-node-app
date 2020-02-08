0d4747a68b979cfa46bcd0bff21175b1d5726a68
borkarsaish65 <borkarsaish7@gmail.com>
2020-02-08 15:26:09 +0530

    Used Object destructuring and object shorthand property

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const geocode = require('./util/geocode');
 const forecast = require('./util/forecast');
+const address = process.argv[2];
 
-geocode('Margao',(error,data)=>{
-console.log('Error',error);
-console.log('Data',data);
-forecast(data.latitude,data.longitude,(error, data) => {
-  console.log('Error', error)
-  console.log('Data', data)
+if(!address)
+{
+  console.log('Please Provide an address');
+
+}
+else {
+geocode(address,(error,{latitude,longitude,location})=>{
+if(error)
+{
+  return console.log(error);
+}
+forecast(latitude,longitude,(error, {summary,temperature,probability}) => {
+ 
+        if(error)
+    {
+        return console.log(error);
+      }
+      console.log(location);
+      console.log('The day will be '+summary+'It will be '+ temperature +' Fareingeight. There is '+probability+'% chance of rain');
 })
 
 })
+
+}
--- util/forecast.js
+++ util/forecast.js
@@ -4,21 +4,21 @@ const forecast = (latitude,longitude,callback) =>{
 
     const url = `https://api.darksky.net/forecast/d375c9bfe82f06660d05faac6f12d15b/${latitude},${longitude}`;
 
-    request({url:url,json:true},(error,response)=>{
+    request({url,json:true},(error,{body})=>{
 
         if(error)
         {
            callback('Unable to connect to the internet',undefined); 
         }
-        else if(response.body.error)
+        else if(body.error)
         {
             callback('Invalid name please retry',undefined);
         }
         else{
             callback(undefined,{
-                summary:response.body.daily.data[0].summary,
-                tempertature:response.body.currently.temperature,
-                probability:response.body.currently.precipProbability
+                summary:body.daily.data[0].summary,
+                temperature:body.currently.temperature,
+                probability:body.currently.precipProbability
             })
         }
     })
